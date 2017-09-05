545d3461db467608d95f3b1908e392b4bb477f9c
Marco Melletti <marco.melletti@t3lab.it>
2017-09-05 11:56:02 +0200

    install script fix

--- install.sh
+++ install.sh
@@ -3,17 +3,23 @@
 MYDIR="$(cd "$(dirname "$BASH_SOURCE[0]")" && pwd)"
 
 backupAndLink(){
-	if test -f "$1" -a -n "$(diff -q "$1" "$2")"; then
-		echo "backing up $1 to $1.orig"
-		mv -f "$1" "$1.orig"
+	if test -f "$1"; then
+		if test -n "$(diff -q "$1" "$2")"; then
+			echo "backing up $1 to $1.orig"
+			mv -f "$1" "$1.orig"
+		elif test -L "$1"; then
+			unlink "$1"
+		fi
 	fi
-	ln -s "$2" "$1"
+	ln -sf "$2" "$1"
 }
 
-backupAndLink "~/.vimrc" "$MYDIR/vimrc"
-backupAndLink "~/.ctags" "$MYDIR/ctags"
+backupAndLink "$HOME/.vimrc" "$MYDIR/vimrc"
+backupAndLink "$HOME/.ctags" "$MYDIR/ctags"
+
+mkdir -p "$HOME/.vim/colors"
 
 ls "$MYDIR/colors" | while read file; do
-	backupAndLink "~/.vim/colors/$(basename "$file")" "$MYDIR/colors/$(basename "$file")"
+	backupAndLink "$HOME/.vim/colors/$(basename "$file")" "$MYDIR/colors/$(basename "$file")"
 done
 
